1afc87719cf63aa00459140c047de486b3b484ca
anykpv <akpv1@aluno.ifal.edu.br>
2025-06-16 17:52:13 -0300

    Adicionando main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'telas/remedios.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaRemedios(),
+    ),
+  );
+}
